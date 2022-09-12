a14b59365b8b22dd010b72cb13b7b8fbef3cc8f7
Vishal Vusirikala <vvusirik@gmail.com>
2022-09-11 21:05:25 -0400

    use api for message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        // 1. Define the async function
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:8000');
+            const result = await response.json();
+            setMessage(result.message);
+        }
+        // 2. Call the async func
+        fetchData();
+    }, []);
+
+    console.log(message);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    {message}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
